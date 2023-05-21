aa7836862de7874272323072492813682ea79f55
Arron Wilcock <arron.wilcock@outlook.com>
2023-05-21 19:25:57 +0100

    home page banner style set

--- my-app/src/styles/components/Banner/Banner.jsx
+++ my-app/src/styles/components/Banner/Banner.jsx
@@ -1,11 +1,10 @@
-import "./Banner.scss";
-import logo from "./src/images.icon-left-font.svg"
+import "./banner.scss";
+import Logo from "../../../images/icon-left-font.svg";
 
 function Banner() {
-  
   return (
     <div className="gm-banner">
-      <img src={logo} alt="Groupomania Logo" className="gm-logo" />
+      <img src={Logo} alt="Groupomania Logo" className="gm-logo" />
       <div className="right-nav">
         <ul>
           <li>
