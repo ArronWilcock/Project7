9f92cf155b0314376413530d1b07bd775550b9d3
Arron Wilcock <arron.wilcock@outlook.com>
2023-05-25 21:33:28 +0100

    backend directory initialized and deps installed

---
+++ my-app/backend/app.js
@@ -0,0 +1,27 @@
+// Required packages and file paths declared
+const express = require("express");
+const axios = require("axios");
+
+// express app is declared by using express like a function
+const app = express();
+
+// middleware that takes incoming requests with content type application/json and makes its body available on the reponse object
+app.use(express.json());
+
+// Middleware to create headers for CORS (cross origin resource sharing)
+// access control for Origin, Headers & Methods declared for the response objects
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// express app exported so that it can be accessed outside the js file
+module.exports = app;
