8e9e77e0557c922430ee7f02eecf6906fcfb5569
Arron Wilcock <arron.wilcock@outlook.com>
2023-08-06 07:49:41 +0100

    login redirect completed

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { store } from "./store.js";
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -7,42 +13,24 @@ import AccountPage from "./pages/Account";
 import CreatePost from "./pages/CreatePost";
 import OnePost from "./pages/SinglePost";
 import PostList from "./components/Post/Posts";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    // Check local storage for login status
-    const isLoggedInStatus = localStorage.getItem("isLoggedIn");
-    if (isLoggedInStatus === "true") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const setLoginState = (loggedIn) => {
-    setIsLoggedIn(loggedIn);
-    localStorage.setItem("isLoggedIn", loggedIn ? "true" : "false");
-  };
-
-  // TODO send user to login page if loggedin = false
+  const { state } = useContext(store);
 
   return (
     <Router>
       <Routes>
-        <Route
-          path="/login"
-          element={<LoginPage setLoginState={setLoginState} />}
-        />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route
           path="/"
-          element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />}
+          element={state.userInfo ? <HomePage /> : <Navigate to="/login" />}
         />
-        <Route path="/" element={<PostList />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/create-post" element={<CreatePost />} />
-        <Route path="/:postId" element={<OnePost />} />
+        <Route path="/" element={state.userInfo ? <PostList /> : <Navigate to="/login" />} />
+        <Route path="/account" element={state.userInfo ? <AccountPage /> : <Navigate to="/login" />} />
+        <Route path="/create-post" element={state.userInfo ? <CreatePost /> : <Navigate to="/login" />} />
+        <Route path="/:postId" element={state.userInfo ? <OnePost /> : <Navigate to="/login" />} />
       </Routes>
     </Router>
   );
